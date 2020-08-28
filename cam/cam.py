d86567fb4a109d75eed6a232883cdc907f670a3e
Fritz Payr <fritz.payr@caplas.at>
2020-08-28 12:10:15 +0200

    First steps...

--- cam/cam.py
+++ cam/cam.py
@@ -1,4 +1,5 @@
-from _signal import pause
+import signal
+from subprocess import Popen
 
 from gpiozero import Button
 from picamera import PiCamera
@@ -7,28 +8,36 @@ from datetime import datetime
 import os
 
 
-btn1 = Button(26)
-btn2 = Button(19)
-btn3 = Button(13)
-btn4 = Button(6)
+def main():
+    btn1 = Button(26)
+    btn2 = Button(19)
+    btn3 = Button(13)
+    btn4 = Button(6)
+    shutter = Button(5)
+    cam = PiCamera()
+    pid = Popen(["fbcp", "&"]).pid
 
-shutter = Button(5)
+    def capture():
+        today = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        cam.capture('/home/pi/{0}.jpg'.format(today))
 
-cam = PiCamera()
+    def stop():
+        raise KeyboardInterrupt
 
-os.system('fbcp &')
+    btn1.when_pressed = cam.start_preview
+    btn2.when_pressed = cam.stop_preview
+    btn4.when_pressed = stop
+    shutter.when_pressed = capture
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        pass
+    finally:
+        cam.stop_preview()
+        os.kill(pid, signal.SIGKILL)
+        cam.close()
 
 
-def capture():
-    today = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    cam.capture('/home/pi/{0}.jpg'.format(today))
-
-
-btn1.when_pressed = cam.start_preview
-btn2.when_pressed = cam.stop_preview
-
-shutter.when_pressed = capture
-
-pause()
-
-cam.close()
+if __name__ == '__main__':
+    main()
